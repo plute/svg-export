1f893ec8794a0d37265e260751f279c830b5b6bf
plute <31183568+plute@users.noreply.github.com>
2017-09-08 23:59:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -138,18 +138,6 @@ var svg = map.append("div")
     .attr("class", "layer")
     .append("svg").attr("id","map").append("g");
 
- var nakagyo = d3.json("./kyoto_city_nakagyo.geojson", function(json) {
-  return svg.attr("class", "nakagyo")
-            .selectAll("path")
-            .data(json.features)
-            .enter()
-            .append("svg:path")
-            .attr("d", tilePath)  //dataに投影法を適応
-            .attr("fill-opacity", 0.5)
-            .attr("fill", "green")
-            .attr("stroke", "#222");
-});
-
 var zoom_controls = map.append("div")
     .attr("class", "zoom-container");
 
@@ -212,6 +200,19 @@ function zoomed() {
       .attr("class", "tile")
       .attr("transform",function(d){ return "translate("+ d[0] * 256 +","+ d[1] * 256 +")"; })
       .each(renderTiles);
+    
+//   var nakagyo = d3.json("./kyoto_city_nakagyo.geojson", function(json) {
+//   return image.append("g")
+//             .attr("class", "nakagyo")
+//             .selectAll("path")
+//             .data(json.features)
+//             .enter()
+//             .append("path")
+//             .attr("d", tilePath)  //dataに投影法を適応
+//             .attr("fill-opacity", 0.5)
+//             .attr("fill", "green")
+//             .attr("stroke", "#222");
+//    });
 }
 
 var download = d3.select("#exportify")
